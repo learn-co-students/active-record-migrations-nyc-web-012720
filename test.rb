9a51d7e9e4021082f4b97a604da9a8e6c24b1172
github username <github email address>
2020-02-14 11:31:19 -0500

    Done.

---
+++ test.rb
@@ -0,0 +1,10 @@
+array = [<Event:0x00007f8222b780f8 id: 15, event_name: "Houston Rodeo w/ Lizzo", venue: "NRG Stadium", date: "03-13", time: "18:45">,
+    <Event:0x00007f8222b83e58 id: 15, event_name: "Houston Rodeo w/ Lizzo", venue: "NRG Stadium", date: "03-13", time: "18:45">,
+    <Event:0x00007f8222b83bb0 id: 15, event_name: "Houston Rodeo w/ Lizzo", venue: "NRG Stadium", date: "03-13", time: "18:45">,
+    <Event:0x00007f8222b838e0 id: 15, event_name: "Houston Rodeo w/ Lizzo", venue: "NRG Stadium", date: "03-13", time: "18:45">,
+    <Event:0x00007f8222b836b0 id: 15, event_name: "Houston Rodeo w/ Lizzo", venue: "NRG Stadium", date: "03-13", time: "18:45">,
+    <Event:0x00007f8222b83458 id: 16, event_name: "Tucson Roadrunners vs San Diego Gulls", venue: "Tucson Arena", date: "02-26", time: "19:05">,
+    <Event:0x00007f8222b83138 id: 16, event_name: "Tucson Roadrunners vs San Diego Gulls", venue: "Tucson Arena", date: "02-26", time: "19:05">,
+    <Event:0x00007f8222b82f30 id: 16, event_name: "Tucson Roadrunners vs San Diego Gulls", venue: "Tucson Arena", date: "02-26", time: "19:05">]
+
+    array[0].event_name
